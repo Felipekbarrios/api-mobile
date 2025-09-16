702012951e1feb5806c9c71cec418e340ca3ec57
Felipekbarrios <barriosfelipek@gmail.com>
2025-09-15 22:51:08 -0300

    primeiro commit

---
+++ components/Home.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import { View, Alert, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { Card, Text, IconButton } from 'react-native-paper';
+import { fetchEstoque, deleteEstoque } from './Api';
+
+export default function Home({ navigation }) {
+  const [registro, setRegistros] = useState([]);
+
+  useEffect(() => {
+    fetchEstoque(setRegistros);
+  }, []);
+
+  const handleDelete = (id) => {
+    Alert.alert(
+      'Confirmação',
+      'Tem certeza de que deseja deletar este item?',
+      [
+        { text: 'Cancelar', style: 'cancel' },
+        {
+          text: 'Deletar',
+          onPress: () => deleteEstoque(id, setRegistros),
+        },
+      ]
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={registro}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Card style={styles.card}>
+            <View style={styles.cardContent}>
+              {/* Coluna da esquerda: Texto */}
+              <View style={styles.infoColumn}>
+                <Text style={styles.title}>Código: {item.id}</Text>
+                <Text>Produto: {item.produto}</Text>
+                <Text>Marca: {item.marca}</Text>
+                <Text>Valor: {item.valor}</Text>
+              </View>
+
+              {/* Coluna da direita: botoes */}
+              <View style={styles.actionColumn}>
+                <IconButton
+                  icon="pencil"
+                  size={24}
+                  iconColor="#3498db"
+                  onPress={() => navigation.navigate('alterar', { Estoque: item })}
+                />
+                <IconButton
+                  icon="delete"
+                  size={24}
+                  iconColor="#e74c3c"
+                  onPress={() => handleDelete(item.id)}
+                />
+              </View>
+            </View>
+          </Card>
+        )}
+      />
+
+        <TouchableOpacity
+            style={styles.floatingButton}
+            onPress={() => navigation.navigate('Cadastro')}
+        
+        >
+        <Text style={styles.plusIcon}>+</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  card: {
+    marginBottom: 5,
+    elevation: 3,
+    borderRadius: 8,
+    backgroundColor: '#fff'
+  },
+  cardContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+  },
+  infoColumn: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  actionColumn: {
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+
+  },
+});
