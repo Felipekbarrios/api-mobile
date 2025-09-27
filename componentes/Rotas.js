64242ae43f69ab1dacf56f2df0d30cc7bb9a7d25
Felipekbarrios <barriosfelipek@gmail.com>
2025-09-26 23:53:44 -0300

    Update Rotas.js

--- componentes/Rotas.js
+++ componentes/Rotas.js
@@ -1,23 +1,18 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '.././Componentes/Home';
+import Cadastro from '.././Componentes/Cadastro';
+import Alterar from '.././Componentes/Alterar';
+import SplashScreen from '.././Componentes/SplashScreen';
 
-import Home from "../componentes/Home";
-import Cadastro from "../componentes/Cadastro";
-import Alterar from "../componentes/Alterar";
-import SplashScreen from "../componentes/SplashScreen";
-
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function Rotas() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerLeft: () => null }} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Alterar" component={Alterar} />
       </Stack.Navigator>
