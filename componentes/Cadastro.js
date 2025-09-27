3f532c9bc99fcc27a56a5827af86984cd2d0dda9
Felipekbarrios <barriosfelipek@gmail.com>
2025-09-26 23:54:30 -0300

    Update Cadastro.js

--- componentes/Cadastro.js
+++ componentes/Cadastro.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, Alert } from "react-native";
-import { createProdutos, updateProdutos } from "./Api";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { createProdutos } from './Api'; // Modificado para './Api' se 'Api' for o nome do arquivo
 
 export default function Cadastro({ navigation }) {
   const [nome, setNome] = useState('');
@@ -14,26 +14,39 @@ export default function Cadastro({ navigation }) {
     }
 
     const newProduto = { nome, marca, preco };
-      const addedProduto = await createProdutos(newProduto);
-      if (addedProduto) {
-        Alert.alert('Sucesso!', 'Cadastro realizado com sucesso!', [
-          { text: 'OK', onPress: () => navigation.navigate('Home') },
-        ]);
-      }
-    }
+    const addedProduto = await createProdutos(newProduto);
 
-    setNome("");
-    setMarca("");
-    setPreco("");
+    if (addedProduto) {
+      Alert.alert('Sucesso!', 'Cadastro realizado com sucesso!', [
+        { text: 'OK', onPress: () => navigation.navigate('Home') },
+      ]);
+
+      // Limpa os campos do formulário
+      setNome('');
+      setMarca('');
+      setPreco('');
+    }
   };
 
   return (
     <View>
-      <TextInput placeholder="Produto" value={nome} onChangeText={setNome} />
-      <TextInput placeholder="Marca" value={marca} onChangeText={setMarca} />
-      <TextInput placeholder="Preco" value={preco} onChangeText={setPreco} />
-
+      <TextInput
+        placeholder="Produto"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        placeholder="Marca"
+        value={marca}
+        onChangeText={setMarca}
+      />
+      <TextInput
+        placeholder="Preço"
+        value={preco}
+        onChangeText={setPreco}
+        keyboardType="numeric" // Adicionado para Preço
+      />
       <Button title="Cadastrar" onPress={handleSubmit} />
     </View>
   );
-
+}
